792da0bda95507cfd358b916c079364dbfc34153
Mubsal <musa2100@student.miun.se>
2024-06-10 17:48:03 +0200

    mergeat

--- app/(dashboard)/dashboard/video/page.tsx
+++ app/(dashboard)/dashboard/video/page.tsx
@@ -4,9 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import BreadCrumb from "@/components/breadcrumb";
 import { Heading } from "@/components/ui/heading";
 import { ScrollArea } from "@/components/ui/scroll-area";
-
+ 
 const breadcrumbItems = [{ title: "Boka samtal", link: "/dashboard/video" }];
-
+ 
 function Page() {
   return (
     <>
@@ -14,17 +14,17 @@ function Page() {
         <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
           <BreadCrumb items={breadcrumbItems} />
           <div className="flex flex-col items-center">
-            <Heading 
-              title="Boka tid" 
-              description="Möt vår erfarna specialpedagog, som med sina 17 års erfarenhet i skolvärlden, är dedikerad till att ge elever med diagnoser möjligheten till en likvärdig skolgång. Boka en tid för att få skräddarsytt stöd och förbättra din studieteknik." 
+            <Heading
+              title="Boka tid"
+              description="Möt vår erfarna specialpedagog, som med sina 17 års erfarenhet i skolvärlden, är dedikerad till att ge elever med diagnoser möjligheten till en likvärdig skolgång. Boka en tid för att få skräddarsytt stöd och förbättra din studieteknik."
             />
           </div>
-
+ 
           {/* Image added from external URL */}
           <div className="flex justify-center">
-            <img src="https://framjaspec.se/____impro/1/onewebmedia/Angelica%20Danielsson%20soffa.jpg" alt="Angelica Danielsson sitting on a sofa" className="w-45 h-48 mb-4" />
+            <img src="https://formd.se/____impro/1/onewebmedia/os_0091_4_8.png" alt="Icon" className="w-45 h-48 mb-4" />
           </div>
-
+ 
           {/* Text and booking button */}
           <div className="flex flex-col items-center text-center px-4">
             <p>Boka ett möte med en specialpedagog för att få skräddarsytt stöd och hjälp.</p>
@@ -32,7 +32,7 @@ function Page() {
             <ol className="list-decimal list-inside mb-4">
               <li>Klicka på knappen nedan för att fortsätta till en säker inloggningssida.</li>
               <p>Du behöver logga in igen med samma inloggning. Vi använder förstärkta säkerhetsåtgärder för videosamtal.</p>
-              <li>Efter inloggning väljer du 'Boka tid'. Du kommer då få en länk.</li>
+              <li>Efter inloggning väljer du 'Boka tid'. Du kommer då få en länk som du kopierar.</li>
               <li>Använd denna länk för att ansluta till mötet. Din specialpedagog kommer att ansluta kort därefter.</li>
               <strong>Vid frågor kontakta oss på boka@formd.se</strong>
             </ol>
@@ -47,5 +47,5 @@ function Page() {
     </>
   );
 }
-
+ 
 export default Page;
